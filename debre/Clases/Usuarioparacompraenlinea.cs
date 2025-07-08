c806f5994f09ff0fb379e5726253dc75fffabba0
Yostin <Yostin@DESKTOP-TPHEBTT>
2025-07-08 00:56:35 -0500

    Agregar archivos de proyecto.

---
+++ debre/Clases/Usuarioparacompraenlinea.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace debre.Clases
+{
+    public class Usuarioparacompraenlinea
+    {
+        public int ID;
+        public string cedula;
+        public string nombredeusuario;
+        public string correoelectronico;
+        public string producto;
+        public string ubicacion;
+        private string tarjetacomprar;
+        private string clavedeacceso;
+
+        public Usuarioparacompraenlinea(int iD, string cedula, string nombredeusuario, string correoelectronico, string producto, string ubicacion, string tarjetacomprar, string clavedeacceso)
+        {
+            ID = iD;
+            this.cedula = cedula;
+            this.nombredeusuario = nombredeusuario;
+            this.correoelectronico = correoelectronico;
+            this.producto = producto;
+            this.ubicacion = ubicacion;
+            this.tarjetacomprar = tarjetacomprar;
+            this.clavedeacceso = clavedeacceso;
+        }
+        public string getTarjeta()
+        {
+            int cantidadCaracteres = this.tarjetacomprar.Length;
+
+            string retornoTarjetaCredito = this.tarjetacomprar.Replace(tarjetacomprar.Substring(0, cantidadCaracteres - 4), "");
+
+            return retornoTarjetaCredito;
+
+        }
+
+        public void setTarjetaCredito(string numeroTarjetaCredito)
+        {
+            Console.WriteLine("Ingrese la clave de acceso: ");
+            string clavedeacceso = Console.ReadLine();
+            if (clavedeacceso == this.clavedeacceso)
+            {
+                this.tarjetacomprar = numeroTarjetaCredito;
+            }
+            else
+            {
+                Console.WriteLine("Clave incorrecta, no tiene acceso a la tarjeta");
+            }
+            }
+             public string getclve()
+        {
+            int cantidadCaracteres = this.tarjetacomprar.Length;
+
+            string retornoTarjetaCredito = this.tarjetacomprar.Replace(tarjetacomprar.Substring(0, cantidadCaracteres - 4), "");
+
+            return retornoTarjetaCredito;
+
+        }
+
+        public void setclave(string numeroTarjetaCredito)
+        {
+            Console.WriteLine("Ingrese la cedula: ");
+            string cedulaIngresada = Console.ReadLine();
+            if (cedulaIngresada == this.cedula)
+            {
+                this.tarjetacomprar = numeroTarjetaCredito;
+            }
+            else
+            {
+                Console.WriteLine("Cedula incorrecta, no tiene acceso a la tarjeta");
+            }
+        }
+    }
+}
---
+++ debre/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using debre.Clases;
+
+namespace debre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuarioparacompraenlinea ObjUsuario = new Usuarioparacompraenlinea(1,"09524680304", "Yostin", "Yostin.hidalgo@tecnologicoliceocristiano", "Gigabyte RTX5090","Vergeles-guayaquil", "18285202", "23014738");
+
+            Console.WriteLine("ID: " + ObjUsuario.ID);
+            Console.WriteLine("Cedula: " + ObjUsuario.cedula);
+            Console.WriteLine("Nombre: " + ObjUsuario.nombredeusuario);
+            Console.WriteLine("Correoelectronico: " + ObjUsuario.correoelectronico);
+            Console.WriteLine("Producto: " + ObjUsuario.producto);
+            Console.WriteLine("Ubicacion: " + ObjUsuario.ubicacion);
+            Console.WriteLine("Tarjeta: " + ObjUsuario.getTarjeta());
+            Console.WriteLine("Clave: " + ObjUsuario.getclve());
+            Console.ReadLine();
+
+
+            instanciarObjeto();
+
+        }
+
+        private static void instanciarObjeto()
+        {
+            Console.Clear();
+            Console.WriteLine("INGRESE UN NUEVO USUARIO");
+            Console.WriteLine();
+            Console.Write("Ingrese lel id: ");
+            int ID = System.Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Ingrese la cedula: ");
+            string cedula = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el nombre de usuario: ");
+            string nombredeusuario = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el Correoelectronico: ");
+            string correoelectronico = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el Producto: ");
+            string producto = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese el Producto: ");
+            string ubicacion = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese su tarjeta: ");
+            string tarjetacomprar = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Ingrese su clave: ");
+            string clavedeacceso = Console.ReadLine();
+            Console.WriteLine();
+
+            Usuarioparacompraenlinea objetoUsuario = new Usuarioparacompraenlinea(ID,cedula, nombredeusuario, correoelectronico, producto,ubicacion, tarjetacomprar, clavedeacceso);
+            Console.WriteLine("La tarjeta de crédito es: " + objetoUsuario.getTarjeta());
+
+            Console.WriteLine("Ingrese la Nueva Tarjeta de Crédito: ");
+            tarjetacomprar = Console.ReadLine();
+
+            objetoUsuario.setTarjetaCredito(tarjetacomprar);
+            Console.WriteLine("La nueva tarjeta de crédito es: " + objetoUsuario.getTarjeta());
+
+            Console.WriteLine("La clave de acceso es: " + objetoUsuario.getclve());
+
+            Console.WriteLine("Ingrese la Nueva Clave de acceso: ");
+            tarjetacomprar = Console.ReadLine();
+
+            objetoUsuario.setTarjetaCredito(tarjetacomprar);
+            Console.WriteLine("La nueva Clave de acceso: " + objetoUsuario.getclve());
+        }
+    }
+}
